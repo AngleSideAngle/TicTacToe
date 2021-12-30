46bc59839b75d4a74dbe1b5cd4ffdba657b338c7
Asa Paparo <asapaparo@gmail.com>
2021-12-30 18:51:43 -0500

    actual commit message

--- Game.java
+++ Game.java
@@ -16,6 +16,86 @@ public class Game {
     }
 
     public void printBoard() {
-        
+        char[][] places = board.getBoard();
+        for (int i = 0; i < places.length; i++) {
+            for (int j = 0; j < places.length; j++) {
+                System.out.print(" " + places + " |");
+            }
+            for (int j = 0; j < places.length; j++)
+                System.out.print("----");
+            System.out.println();
+        }
+    }
+
+
+    /*
+    public char[][] getBoard(String[][] board, String[][] input) {
+        int r = 0;
+        int c = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+            if (j % 2 == 0 && i % 2 == 0) {
+                board[i][j] = input[r][c];
+                c++;
+            }
+            System.out.print(board[i][j]);
+        }
+        c = 0;
+        if (i % 2 == 0) {
+            r++;
+        }
+        System.out.println();
+        }
+    }
+    */
+     
+
+    public boolean checkWin(String[][] board, String letter) {
+        int count = 0;
+        for (int i = 0; i < board.length; i += 2) {
+            for (int j = 0; j < board[i].length; j += 2) {
+                if (board[i][j].equals(letter)) {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+            count = 0;
+        }
+        for (int i = 0; i < board.length; i += 2) {
+            for (int j = 0; j < board[i].length; j += 2) {
+                if (board[j][i].equals(letter)) {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+            count = 0;
+        }
+
+        for (int i = 0; i < board.length; i += 2) {
+            for (int j = i; j <= i; j++) {
+                if (board[i][j].equals(letter)) {
+                    count++;
+                }
+            }
+        }
+        if (count == 3) {
+            return true;
+        }
+        count = 0;
+        for (int i = board.length - 1; i >= 0; i -= 2) {
+            for (int j = 4 - i; j <= 4 - i; j++) {
+                if (board[i][j].equals(letter)) {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+        }
+        return false;
     }
 }
