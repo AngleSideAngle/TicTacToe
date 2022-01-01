4334fe981afd509b7d69fef63976453d4568ccef
Asa Paparo <asapaparo@gmail.com>
2022-01-01 17:02:37 -0500

    fixed scanner bug

--- Human.java
+++ Human.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
 public class Human {
     private Player player;
-    private Scanner sc;
 
     public Human() {
         player = new Player('X');
-        sc = new Scanner(System.in);
     }
 
     public void move(Board board) {
@@ -21,7 +17,7 @@ public class Human {
     public int[] prompt() {
         int[] coords = new int[2];
         System.out.print("Enter X,Y coordinates for your move: ");
-        String pair = sc.nextLine();
+        String pair = Main.sc.nextLine();
         coords[0] = Integer.parseInt(pair.substring(0, 1)) - 1;
         coords[1] = Integer.parseInt(pair.substring(2)) - 1;
         return coords;
--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 class Main {
+    public static Scanner sc;
     public static void main(String[] args) {
+        sc = new Scanner(System.in);
         // main game loop
         do {
             Game game = new Game();
             while (game.round());
         } while (playAgain());
+        sc.close();
     }
+    // more code
 
     public static boolean playAgain() {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Would you like to play again? Enter 1 for yes, 0 for no:\n> ");
         String answ = sc.nextLine();
-        sc.close();
         if (answ.equals("1"))
             return true;
         return false;
