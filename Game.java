ce6fe89daffc48bf8ba9fe8d899c029e9a569c66
Asa Paparo <asapaparo@gmail.com>
2022-01-01 16:07:24 -0500

    fixed win conditions

--- Game.java
+++ Game.java
@@ -43,16 +43,16 @@ public class Game {
     public char checkWin(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             // rows
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
                 return board[i][0];
             // columns
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
                 return board[0][i];
         }
         // diagonals
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
             return board[0][0];
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
             return board[0][2];
         return ' ';
     }
